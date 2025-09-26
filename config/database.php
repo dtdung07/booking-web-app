d4afc019cb4a0bb0cb2aceb734ac8bc3eba1a58e
tinvu <vutin686@gmail.com>
2025-09-26 16:20:40 +0700

    remove config gitignore

--- config/database.php
+++ config/database.php
@@ -5,19 +5,30 @@ class Database {
     private $username = "root";
     private $password = "rootpassword";
     public $conn;
-   public function getConnection() {
+    public function getConnection() {
     $this->conn = null;
+    try {
+        // Tạo kết nối MySQLi
+        $this->conn = new mysqli(
+            $this->host,
+            $this->username,
+            $this->password,
+            $this->db_name
+        );
 
-    // Tạo kết nối MySQLi
-    $this->conn = new mysqli($this->host, $this->username, $this->password, $this->db_name);
+        // Kiểm tra lỗi kết nối
+        if ($this->conn->connect_error) {
+            throw new Exception("Lỗi kết nối: " . $this->conn->connect_error);
+        }
 
-    // Kiểm tra lỗi kết nối
-    if ($this->conn->connect_error) {
-        die("Kết nối thất bại: " . $this->conn->connect_error);
+        // Đặt charset UTF-8
+        if (!$this->conn->set_charset("utf8")) {
+            throw new Exception("Không thể set charset UTF-8: " . $this->conn->error);
+        }
+
+    } catch (Exception $exception) {
+        echo "Lỗi kết nối: " . $exception->getMessage();
     }
-
-    // Thiết lập charset UTF-8
-    $this->conn->set_charset("utf8");
 
     return $this->conn;
 }
