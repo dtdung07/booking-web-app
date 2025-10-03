ffddc91b532af2ed9c033207d4eae25beb420dab
tuanbao2 <https://github.com/tuanbao2>
2025-10-03 23:46:29 +0700

    a

--- app/views/admin/categories/connect.php
+++ app/views/admin/categories/connect.php
@@ -2,7 +2,7 @@
     $host = 'localhost';
     $user = 'root';
     $pass = '';
-    $database = 'booking_restaurant';
+    $database = 'booking_web_app';
     $port = '3306';
 
     $conn = mysqli_connect($host, $user, $pass, $database, $port);
